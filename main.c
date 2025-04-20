18bbafde78250ced413acc0c2f1d92863b1dc6b0
vrnssx <diana.pasishnicenko@gmail.com>
2025-04-20 22:07:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,41 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-#define SIZE 10
+#define SIZE 5
 
 int main() {
-    double array[SIZE];
-    double sum = 0.0, average;
-    int i, closest_index;
-    double min_diff;
-    int first_negative_index = -1, second_positive_index = -1;
-    double sum_between = 0.0;
+    int matrix[SIZE][SIZE];
 
-    printf("Введіть %d дійсних чисел:\n", SIZE);
-    for (i = 0; i < SIZE; i++) {
-        scanf("%lf", &array[i]);
-        sum += array[i];
-    }
-
-    average = sum / SIZE;
-
-    closest_index = 0;
-    min_diff = fabs(array[0] - average);
-    for (i = 1; i < SIZE; i++) {
-        double diff = fabs(array[i] - average);
-        if (diff < min_diff) {
-            min_diff = diff;
-            closest_index = i;
+    printf("Введіть елементи квадратної матриці розміром %dx%d:\n", SIZE, SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("Елемент [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
         }
     }
 
-    for (i = 0; i < SIZE; i++) {
-        if (array[i] < 0 && first_negative_index == -1) {
-            first_negative_index = i;
+    printf("\nВведена матриця:\n");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d\t", matrix[i][j]);
         }
-        if (array[i] > 0 && first_negative_index != -1 && second_positive_index == -1 && i > first_negative_index) {
-            second_positive_index = i;
-            break;
+        printf("\n");
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (matrix[i][j] % 3 == 0) {
+                matrix[i][j] = 3;
+            }
         }
     }
 
-    if (first_negative_index != -1 && second_positive_index != -1 && second_positive_index > first_negative_index) {
-        for (i = first_negative_index + 1; i < second_positive_index; i++) {
-            sum_between += array[i];
+    printf("\nМатриця після заміни:\n");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d\t", matrix[i][j]);
         }
-    }
-
-    printf("Номер елемента, найближчого до середнього арифметичного: %d\n", closest_index);
-    if (first_negative_index != -1 && second_positive_index != -1 && second_positive_index > first_negative_index) {
-        printf("Сума елементів між першим від'ємним та другим додатним: %.2lf\n", sum_between);
-    } else {
-        printf("Не вдалося знайти перший від'ємний та другий додатний елементи, або вони розташовані неправильно.\n");
+        printf("\n");
     }
 
     return 0;
